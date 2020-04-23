d811d8860dc10e62cf95129516cea0f4723495d6
rdlne17 <brandonwagner123@outlook.com>
2020-04-22 23:38:08 -0500

    Added basic movement and movement validation

--- controller/LevelController.java
+++ controller/LevelController.java
@@ -7,11 +7,15 @@ import java.util.ResourceBundle;
 
 import application.model.Enemy;
 import application.model.Item;
+import application.model.Levels;
 import application.model.Player;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
 
 public class LevelController implements Initializable{
 	
@@ -30,8 +34,18 @@ public class LevelController implements Initializable{
 	@FXML
 	public ProgressBar currentHpBar=null;
 	
+	@FXML
+	public Circle startCircle, pos10, pos11, pos1_1, pos20, pos21, pos22, pos2_1, pos2_2, pos30, pos31, pos32, pos33, pos3_1,
+				pos3_2, pos3_3, pos40, pos41, pos42, pos43, pos44, pos4_1, pos4_2, pos4_3, pos4_4, pos50, pos51, pos52, pos53,
+				pos5_1, pos5_2, pos5_3, pos60, pos61, pos62, pos6_1, pos6_2, pos70, pos71, pos7_1, pos80, finishCircle;
+	
+	private Circle currentPos, tmp;
+	
 	private Enemy e;
 	private Player p;
+	private Levels l;
+	
+	Circle layout_Circle[][];
 	
 	int layout[][]={{-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
 					{-1, -1, -1, -1, -1,  1, -1, -1, -1, -1, -1},
@@ -131,6 +145,105 @@ public class LevelController implements Initializable{
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		popEnemies();
+		setCircle();
+		
+		l=new Levels(layout);
+		
+		setValidMoves();
+		setCurrentPos();
+		
+	}
+	
+	private void setValidMoves() {
+		l.getValidMoves();
+		
+		if(l.upValid) {
+			tmp=layout_Circle[l.y-1][l.x];
+			tmp.setFill(Paint.valueOf("Green"));
+		}
+			
+		if(l.leftValid) {
+			tmp=layout_Circle[l.y][l.x-1];
+			tmp.setFill(Paint.valueOf("Green"));
+		}
+			
+		if(l.rightValid) {
+			tmp=layout_Circle[l.y][l.x+1];
+			tmp.setFill(Paint.valueOf("Green"));
+		}
+	}
+	
+	private void setCircle() {
+		layout_Circle=new Circle[][] {{null,   null,   null,   null,   null,         null,  null,  null,  null,  null, null},
+			   						  {null,   null,   null,   null,   null, finishCircle,  null,  null,  null,  null, null},
+			   						  {null,   null,   null,   null,   null,        pos80,  null,  null,  null,  null, null},
+			   						  {null,   null,   null,   null, pos7_1,        pos70, pos71,  null,  null,  null, null},
+			   						  {null,   null,   null, pos6_2, pos6_1,        pos60, pos61, pos62,  null,  null, null},
+			   						  {null,   null, pos5_3, pos5_2, pos5_1,        pos50, pos51, pos52, pos53,  null, null},
+			   						  {null, pos4_4, pos4_3, pos4_2, pos4_1,        pos40, pos41, pos42, pos43, pos44, null},
+			   						  {null,   null, pos3_3, pos3_2, pos3_1,        pos30, pos31, pos32, pos33,  null, null},
+			   						  {null,   null,   null, pos2_2, pos2_1,        pos20, pos21, pos22,  null,  null, null},
+			   						  {null,   null,   null,   null, pos1_1,        pos10, pos11,  null,  null,  null, null},
+			   						  {null,   null,   null,   null,   null,  startCircle,  null,  null,  null,  null, null},
+			   						  {null,   null,   null,   null,   null,         null,  null,  null,  null,  null, null}};
+	}
+	
+	@FXML
+	private void moveLeft(ActionEvent event) {
+		if(l.leftValid) {
+			unsetValidMoves();
+			l.moveLeft();
+			setCurrentPos();
+		}else {
+			return;
+		}
+	}
+	
+	@FXML
+	private void moveRight(ActionEvent event) {
+		if(l.rightValid) {
+			unsetValidMoves();
+			l.moveRight();
+			setCurrentPos();
+		}else {
+			return;
+		}
+	}
+	
+	@FXML
+	private void moveUp(ActionEvent event) {
+		if(l.upValid) {
+			unsetValidMoves();
+			l.moveUp();
+			setCurrentPos();
+		}else {
+			return;
+		}
+	}
+	
+	private void unsetValidMoves() {
+		currentPos.setFill(Paint.valueOf("Dodgerblue"));
+		
+		if(l.upValid) {
+			tmp=layout_Circle[l.y-1][l.x];
+			tmp.setFill(Paint.valueOf("Dodgerblue"));
+		}
+		
+		if(l.leftValid) {
+			tmp=layout_Circle[l.y][l.x-1];
+			tmp.setFill(Paint.valueOf("Dodgerblue"));
+		}
+			
+		if(l.rightValid) {
+			tmp=layout_Circle[l.y][l.x+1];
+			tmp.setFill(Paint.valueOf("Dodgerblue"));}
+	}
+	
+	private void setCurrentPos() {
+		currentPos=layout_Circle[l.y][l.x];
+		currentPos.setFill(Paint.valueOf("Red"));
+		
+		setValidMoves();
 	}
 
 }
---
+++ model/Levels.java
@@ -0,0 +1,56 @@
+package application.model;
+
+import javafx.scene.shape.Circle;
+
+public class Levels {
+	private int level[][];
+	public int x;
+	public int y;
+	public boolean upValid;
+	public boolean leftValid;
+	public boolean rightValid;
+	
+	public Levels(int l[][]) {
+		level=l;
+		getCurrentPos();
+		getValidMoves();
+	}
+	
+	private void getCurrentPos() {
+		for(int y=0; y<level.length; y++) {
+			for(int x=0; x<level[y].length; x++) {
+				if(level[y][x]==0) {
+					this.x=x;
+					this.y=y;
+					System.out.println(x+" "+y);
+				}
+			}
+		}
+	}
+	
+	public void getValidMoves() {
+		upValid=false; leftValid=false; rightValid=false;
+		
+		if(level[y-1][x]!=-1)
+			upValid=true;
+		
+		if(level[y][x+1]!=-1)
+			rightValid=true;
+		
+		if(level[y][x-1]!=-1)
+			leftValid=true;
+	}
+	
+	public void moveLeft() {
+		x-=1;
+	}
+	
+	public void moveUp() {
+		y-=1;
+	}
+	
+	public void moveRight() {
+		x+=1;
+	}
+
+}
